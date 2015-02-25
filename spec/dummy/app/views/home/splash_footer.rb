0cfb0b41d82c36b0dabc79df72b101e4e7032886
ajb <ad@mbecker.cc>
2015-02-25 17:34:42 -0500

    use our grid...

--- spec/dummy/app/views/home/splash_footer.rb
+++ spec/dummy/app/views/home/splash_footer.rb
@@ -1,60 +1,71 @@
 class Views::Home::SplashFooter < Views::Splash
   def main
-    section.main_section.footer {
-      div.grid {
-        form.newsletter_form {
-          p {
+    footer.footer {
+      div.footer_inner {
+        form.newsletter_form.grid.gutter_half {
+          p.item {
             strong 'Subscribe to our newsletter!'
             text " We'll email you around once a month with updates."
           }
-          label 'Enter your email address', for: 'newsletter_email'
-          input type: 'email', id: 'newsletter_email', placeholder: 'Enter your email address here', class: 'newsletter_email'
-          input type: 'submit', value: 'Subscribe', class: 'button newsletter_subscribe'
+
+          div.item.lap_seven_columns {
+            label 'Enter your email address', for: 'newsletter_email'
+            input type: 'email', id: 'newsletter_email', placeholder: 'Enter your email address here', class: 'newsletter_email'
+          }
+          div.item.lap_five_columns {
+            input type: 'submit', value: 'Subscribe', class: 'button newsletter_subscribe'
+          }
         }
 
-        ul.footer_links {
-          li {
-            a 'Blog', href: 'http://blog.dobt.co'
-          }
-          li.footer_status {
-            a 'Status Board', href: 'http://status.dobt.co'
-          }
-          li {
-            a 'Terms of Service', href: 'https://dashboard.dobt.co/terms'
-          }
-          li {
-            a 'Privacy Policy', href: 'https://dashboard.dobt.co/privacy'
-          }
-          li {
-            a 'Customer Dashboard', href: 'https://dashboard.dobt.co'
-          }
-        }
-        ul.footer_contact {
-          li.footer_phone {
-            dl {
-              dt 'Toll-Free'
-              dd {
-                a '+1 (877) 294-DOBT', href: 'tel:+18772943628'
+        div.grid {
+          div.item.lap_six_columns {
+            ul.footer_links {
+              li {
+                a 'Blog', href: 'http://blog.dobt.co'
               }
-            }
-            dl {
-              dt 'Local'
-              dd {
-                a '+1 (404) 975-1962', href: 'tel:+14049751962'
+              li.footer_status {
+                a 'Status Board', href: 'http://status.dobt.co'
+              }
+              li {
+                a 'Terms of Service', href: 'https://dashboard.dobt.co/terms'
+              }
+              li {
+                a 'Privacy Policy', href: 'https://dashboard.dobt.co/privacy'
+              }
+              li {
+                a 'Customer Dashboard', href: 'https://dashboard.dobt.co'
               }
             }
           }
-          li.footer_email {
-            a.dynamic_email
-          }
-          li.footer_fb {
-            a 'Facebook', href: 'https://www.facebook.com/dobtco'
-          }
-          li.footer_gplus {
-            a 'Google&#43;'.html_safe, href: 'https://plus.google.com/+DobtCo'
-          }
-          li.footer_twitter {
-            a 'Twitter', href: 'https://www.twitter.com/dobtco'
+          div.item.lap_six_columns {
+            ul.footer_contact {
+              li.footer_phone {
+                dl {
+                  dt 'Toll-Free'
+                  dd {
+                    a '+1 (877) 294-DOBT', href: 'tel:+18772943628'
+                  }
+                }
+                dl {
+                  dt 'Local'
+                  dd {
+                    a '+1 (404) 975-1962', href: 'tel:+14049751962'
+                  }
+                }
+              }
+              li.footer_email {
+                a.dynamic_email
+              }
+              li.footer_fb {
+                a 'Facebook', href: 'https://www.facebook.com/dobtco'
+              }
+              li.footer_gplus {
+                a 'Google&#43;'.html_safe, href: 'https://plus.google.com/+DobtCo'
+              }
+              li.footer_twitter {
+                a 'Twitter', href: 'https://www.twitter.com/dobtco'
+              }
+            }
           }
         }
       }
