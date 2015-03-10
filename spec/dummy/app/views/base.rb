d10e056ab6a6b5c533e555996ae671cc134b6dd5
ajb <ad@mbecker.cc>
2015-03-10 17:04:52 -0400

    rename class

--- spec/dummy/app/views/base.rb
+++ spec/dummy/app/views/base.rb
@@ -101,7 +101,7 @@ class Views::Base < Erector::Widget
 
                 div.dropdown_menu {
                   ul.dropdown_body {
-                    li.dropdown_header.dropdown_header_strong 'My account'
+                    li.dropdown_header.dropdown_header_nav 'My account'
                     li { a 'Edit profile', href: '#' }
                     li { a 'Sign out', href: '#' }
                   }
