599df4d63ddc2b9ea719635acd4509d1897aaac5
Aviv Nitsan <aviv@dobt.co>
2015-05-11 10:43:51 -0700

    add 'alert' and 'notice' flashes for devise

--- lib/dvl/components/flashes.rb
+++ lib/dvl/components/flashes.rb
@@ -4,7 +4,7 @@ class Dvl::Components::Flashes < Erector::Widget
   needs :flash
 
   def content
-    @flash.select { |k, v| k.to_s.in?(%w(success error info)) && v.present? }.each do |k, v|
+    @flash.select { |k, v| k.to_s.in?(%w(success error info alert notice)) && v.present? }.each do |k, v|
       div(class: "flash flash_#{k}") {
         a.flash_close '&times;'.html_safe
         span text v
