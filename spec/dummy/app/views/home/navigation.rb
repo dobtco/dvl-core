96fd34ccd5b0db17c07bbb0519e58ebaeb071cd8
Adam Becker <ad@mbecker.cc>
2015-05-14 21:07:59 -0400

    update preview page

--- spec/dummy/app/views/home/navigation.rb
+++ spec/dummy/app/views/home/navigation.rb
@@ -19,6 +19,12 @@ class Views::Home::Navigation < Views::Base
                 li {
                   a 'Something else'
                 }
+                li {
+                  a {
+                    i(class: 'fa fa-bolt navbar_icon')
+                    span.navbar_badge '1'
+                  }
+                }
               }
             }
             div.navbar_content.navbar_content_secondary {
@@ -35,33 +41,7 @@ class Views::Home::Navigation < Views::Base
       }
     }, full: true
 
-    docs 'Navbar Badge', %{
-      nav.navbar {
-        div.container {
-          div.navbar_header {
-            a.navbar_brand(href: '#') {
-              img src: 'http://www.dobt.co/img/dobt_logo.png'
-            }
-            a.navbar_toggle "<i class='fa fa-reorder'></i>".html_safe
-          }
-
-          div.navbar_content_wrapper {
-            div.navbar_content.navbar_content_primary {
-              ul {
-                li {
-                  a {
-                    i(class: 'fa fa-bolt navbar_icon')
-                    span.navbar_badge '1'
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }, full: true
-
-    docs 'Page Header with Secondary and Tertiary Nav and Subheader', %{
+    docs 'Page Header with Secondary and Tertiary Nav', %{
       div.page_header {
         h2 'Page header'
 
@@ -81,19 +61,29 @@ class Views::Home::Navigation < Views::Base
 
         ul.page_header_tertiary_nav {
           li {
-            a 'Tertiary nav'
+            a {
+              i(class: 'fa fa-pencil')
+              text 'Tertiary nav'
+            }
           }
 
           li {
-            a 'A thing'
+            a {
+              i(class: 'fa fa-file')
+              text 'A thing'
+            }
           }
 
           li {
-            a 'Another thing'
+            a {
+              text 'Another thing'
+            }
           }
         }
       }
+    }, full: true
 
+    docs 'Page subheader and actions', %{
       div.page_subheader {
         h3 'Subheader'
 
