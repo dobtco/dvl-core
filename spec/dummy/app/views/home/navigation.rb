adc427aa4272646aa418e5061125af9726907a33
Adam Becker <ad@mbecker.cc>
2015-05-21 10:38:36 -0400

    Add more page header styles

--- spec/dummy/app/views/home/navigation.rb
+++ spec/dummy/app/views/home/navigation.rb
@@ -46,44 +46,91 @@ class Views::Home::Navigation < Views::Base
         h2 {
           a 'Page header', href: '#'
         }
+        div.page_header_secondary {
+          ul {
+            li {
+              a 'Secondary nav'
+            }
 
-        ul.page_header_secondary_nav {
-          li {
-            a 'Secondary nav'
-          }
+            li {
+              a 'A thing'
+            }
 
-          li {
-            a 'A thing'
-          }
-
-          li {
-            a 'Another thing'
+            li {
+              a 'Another thing'
+            }
           }
         }
-
-        ul.page_header_tertiary_nav {
-          li {
-            a {
-              i(class: 'fa fa-pencil')
-              text 'Tertiary nav'
+        div.page_header_tertiary {
+          ul {
+            li {
+              a {
+                i(class: 'fa fa-pencil')
+                text 'Tertiary nav'
+              }
             }
-          }
 
-          li {
-            a {
-              i(class: 'fa fa-file')
-              text 'A thing'
+            li {
+              a {
+                i(class: 'fa fa-file')
+                text 'A thing'
+              }
             }
-          }
 
-          li {
-            a {
-              text 'Another thing'
+            li {
+              a {
+                text 'Another thing'
+              }
             }
           }
         }
       }
     }, full: true, hint: '<h2> can optionally contain an <a> tag inside.'
+
+    docs 'Page Header with button and back arrow', %{
+      div.page_header.with_back_arrow {
+        a.page_header_back_arrow(title: 'Back to account', 'data-toggle' => 'tooltip') {
+          i(class: 'fa fa-arrow-circle-o-left')
+        }
+
+        h2 {
+          a 'Page header', href: '#'
+        }
+
+        div.page_header_secondary {
+          ul {
+            li {
+              a 'Secondary nav'
+            }
+
+            li {
+              a 'A thing'
+            }
+
+            li {
+              a 'Another thing'
+            }
+          }
+        }
+        div.page_header_tertiary {
+          ul {
+            li {
+              a(href: '#') {
+                text 'A thing'
+              }
+            }
+
+            li {
+              a(href: '#') {
+                text 'Another thing'
+              }
+            }
+          }
+
+          a.button.info.long_arrow 'View project', href: '#'
+        }
+      }
+    }, full: true
 
     docs 'Page subheader (h3) and actions', %{
       div.page_subheader {
