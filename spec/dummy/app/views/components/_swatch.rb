299b1d9107ffd639a702acb8e5eba22a63e6ee2f
Adam Becker <ad@mbecker.cc>
2015-07-02 13:28:08 -0400

    erector cleanup
    @jrubenoff: if a parameter is required, it should be specified without
    ": nil"

--- spec/dummy/app/views/components/_swatch.rb
+++ spec/dummy/app/views/components/_swatch.rb
@@ -1,9 +1,9 @@
 class Views::Components::Swatch < Erector::Widget
-  needs color: nil,
-        var: nil
+  needs :color,
+        :var
 
   def content
-    div.swatch{
+    div.swatch {
       div(style: "background-color: #{@color}")
       span "#{@var}"
     }
