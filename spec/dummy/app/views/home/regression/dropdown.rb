937f7663d08769c79faf000b11fc4e201484bf13
Adam Becker <ad@mbecker.cc>
2015-09-14 12:46:30 -0400

    add more regression testing

---
+++ spec/dummy/app/views/home/regression/dropdown.rb
@@ -0,0 +1,28 @@
+class Views::Home::Regression::Dropdown < Views::Base
+  def render_body
+    div.dropdown {
+      a.button.toggle(
+        'Dropdown',
+        'data-toggle' => 'dropdown',
+        href: '#'
+      )
+
+      div.dropdown_menu(role: 'menu') {
+        ul.dropdown_body {
+          li {
+            a 'Option 1', href: '#'
+          }
+          li.divider {
+            a 'Option 2, with divider'
+          }
+          li {
+            a(href: 'mailto:support@dobt.co') {
+              span.drop_rt_item 'Contact support&hellip;'.html_safe
+              span.drop_rt_arrow { i(class: 'fa fa-external-link') }
+            }
+          }
+        }
+      }
+    }
+  end
+end
