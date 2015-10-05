b67728457c16f4f2abf7f6c491e7d81ff224c312
Adam Becker <ad@mbecker.cc>
2015-10-05 18:25:34 -0400

    add regression test to aid commenting...

---
+++ spec/dummy/app/views/home/regression/header_two.rb
@@ -0,0 +1,26 @@
+class Views::Home::Regression::HeaderTwo < Views::Base
+  def stylesheets
+    super
+
+    style %{
+      body {
+        padding: 50px;
+      }
+    }.html_safe
+  end
+
+  def render_body
+    div.page_header.with_back_arrow.with_pagination {
+      a.page_header_back_arrow(title: 'All responses', 'data-toggle' => 'tooltip') {
+        i(class: 'fa fa-arrow-circle-o-left')
+      }
+
+      h2 {
+        a.page_header_context '2015 Civic Innovation Conference', href: '#'
+        text 'Max Ophüls'
+      }
+    }
+
+    a 'Go to first page', href: '/regression/header_one'
+  end
+end
