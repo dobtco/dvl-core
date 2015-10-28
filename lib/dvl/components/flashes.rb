ab1c0f66d6142f2443ff84796899304983d29470
Adam Becker <ad@mbecker.cc>
2015-10-28 08:15:59 -0700

    allow for server-side flashes with links

--- lib/dvl/components/flashes.rb
+++ lib/dvl/components/flashes.rb
@@ -4,9 +4,18 @@ class Dvl::Components::Flashes < Erector::Widget
   needs :flash
 
   def content
-    @flash.select { |k, v| k.to_s.in?(%w(success error info)) && v.present? }.each do |k, v|
-      script %{DvlFlash("#{k}", "#{h(v)}")}.html_safe
+    selected_flashes.each do |k, v|
+      if v.is_a?(Array)
+        text, links = v
+      else
+        text = v
+      end
+
+      script %{DvlFlash("#{k}", "#{h(text)}", "#{escape_javascript(links)}")}.html_safe
     end
   end
-end
 
+  def selected_flashes
+    @flash.select { |k, v| k.to_s.in?(%w(success error info)) && v.present? }
+  end
+end
