e1521b7dee10e5d5f30f1441c8c5c94964e460a8
Adam Becker <ad@mbecker.cc>
2016-05-22 20:36:10 -0700

    fix the displayed line height value
    closes #241
    cherry-picked from #250

--- spec/dummy/app/views/home/index.rb
+++ spec/dummy/app/views/home/index.rb
@@ -34,7 +34,7 @@ class Views::Home::Index < Views::Home
       div(class: 'dvlcore_headings') {
         h1 'Heading 1 56/80px'
         h2 'Heading 2 36/56px'
-        h3 'Heading 3 23/32px'
+        h3 'Heading 3 23/36px'
         h4 'Heading 4 16/24px'
         h5 'Heading 5 13/24px'
       }
