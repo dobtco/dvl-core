52c2ffc0c730b8d5b0a77ef1cfbea3e4c953ef12
Adam Becker <ad@mbecker.cc>
2016-06-29 20:50:14 -0700

    progress....

--- spec/dummy/app/views/home/regression/selectize.rb
+++ spec/dummy/app/views/home/regression/selectize.rb
@@ -2,10 +2,22 @@ class Views::Home::Regression::Selectize < Views::Layout
   def render_body
     div(class: "container") {
       div(class: "form_item") {
+        label 'Native', for: 'select2'
+
+        select(id: 'select2', 'data-width' => 'full') {
+          option 'Select a page...', value: ''
+          Rails.configuration.x.pages.each do |name, path|
+            option(name, value: path)
+          end
+        }
+      }
+
+      div(class: "form_item") {
         label 'Selectize (single)', for: 'select1'
 
         select('data-no-styled-select' => true, id: 'select1') {
           option 'Select a page...', value: ''
+          option('Howdy howdy howdy howdyu howdy howdyho wdhiuwod', value: 'asdf', selected: true)
           Rails.configuration.x.pages.each do |name, path|
             option(name, value: path)
           end
