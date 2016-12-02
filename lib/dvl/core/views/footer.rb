92df2543ae23ac7dfd6743bc8ffdfb37e754a007
Adam Becker <ad@mbecker.cc>
2016-12-01 18:11:12 -0800

    add append_links and prepend_links to footer
    cf #4355

--- lib/dvl/core/views/footer.rb
+++ lib/dvl/core/views/footer.rb
@@ -1,6 +1,8 @@
 class Dvl::Core::Views::Footer < Dvl::Core::Views.base_view_class.constantize
   needs :application_name,
-        append: nil
+        append: nil,
+        prepend_links: nil,
+        append_links: nil
 
   def content
     footer(class: 'footer') {
@@ -15,16 +17,17 @@ class Dvl::Core::Views::Footer < Dvl::Core::Views.base_view_class.constantize
         }
 
         ul {
+          @prepend_links.call if @prepend_links
           li { a t('dvl_core.footer.status'), href: 'http://status.dobt.co', target: '_blank' }
           li { a t('dvl_core.footer.legal'), href: 'https://www.dobt.co/terms/', target: '_blank' }
           li { a t('dvl_core.footer.help'), href: 'http://help.dobt.co', target: '_blank' }
           li { a t('dvl_core.footer.contact'), href: 'mailto:support@dobt.co' }
+          @append_links.call if @append_links
         }
       }
 
-      if @append
-        @append.call
-      end
+
+      @append.call if @append
     }
   end
 end
